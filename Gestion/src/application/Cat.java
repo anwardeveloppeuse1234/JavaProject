cf797914d2971b4c4ccaa7de8f27a458274bb8a0
anwardeveloppeuse1234 <98185212+anwardeveloppeuse1234@users.noreply.github.com>
2022-06-01 11:34:06 +0200

    mini_projet

---
+++ Gestion/src/application/Cat.java
@@ -0,0 +1,20 @@
+package application;
+
+public class Cat {
+
+	String catego;
+
+	public String getCatego() {
+		return catego;
+	}
+
+	public void setCatego(String catego) {
+		this.catego = catego;
+	}
+
+	public Cat(String catego) {
+		super();
+		this.catego = catego;
+	}
+	
+}
